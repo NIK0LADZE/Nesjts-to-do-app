e79488420d712b1f243e97e9160300558d4518c3
NIK0LADZE <giorgi.nikoladze@scandiweb.com>
2023-01-07 18:22:29 +0400

    fetchHelper utility was updated

--- apps/to-do-app/src/app/Utils/fetchHelper.tsx
+++ apps/to-do-app/src/app/Utils/fetchHelper.tsx
@@ -5,16 +5,15 @@ type Method = 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
 interface FetchOptions {
     fetchUrl: string;
     method: Method,
-    withAccessToken?: boolean,
     formData?: object;
     signal?: AbortSignal
 }
 
-export const fetchHelper = ({ fetchUrl, method, formData, withAccessToken = true, signal }: FetchOptions): Promise<Response> => {
+export const fetchHelper = ({ fetchUrl, method, formData, signal }: FetchOptions): Promise<Response> => {
     const accessToken = localStorage.getItem(ACCESS_TOKEN);
     const headers = new Headers();
     headers.set('Content-Type', 'application/json');
-    if (withAccessToken) headers.set('Authorization', `Bearer ${ accessToken }`)
+    if (accessToken) headers.set('Authorization', `Bearer ${ accessToken }`)
 
     return fetch(fetchUrl, {
         signal,
